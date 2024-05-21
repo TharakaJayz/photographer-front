fb643d94950af9b05355e41cb38913bda6218947
Tharaka Prabhath <tharakaprabhath300@gmail.com>
2024-05-21 18:38:23 +0530

    changes

--- src/components/home/HomeReviewDesktop.tsx
+++ src/components/home/HomeReviewDesktop.tsx
@@ -35,22 +35,101 @@ const HomeReviewDesktop = (props: Props) => {
                         
                         <div className='flex  h-full w-full   items-center '>
                             <section className='flex items-start text-white '>
-                                <span className='text-9xl'>"</span>
+                                {/* <span className='text-9xl'>"</span> */}
                                 <div className='flex flex-col gap-vh4 '>
                                     <p className='text-xl'>Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.</p>
                                     <h1 className='text-4xl font-bold'>Randi & Ishan</h1>
                                 </div>
                             </section>
-                            <section className='  w-full h-full  relative'>
-                                <img src={images[0]} className='absolute top-0 right-0 h-full w-p65 object-cover rounded-3xl object-center' alt='img_1'  />
-                                <img src={images[1] }  className='absolute bottom-vh5 left-vw2 w-p40 h-p50 object-cover object-center rounded-2xl'  alt='img_1'  />
+                            <section className='  w-full h-full  relative border'>
+                                {/* <img src={images[0]} className='absolute top-0 right-0 h-full w-p65 object-cover rounded-3xl object-center' alt='img_1'  />
+                                <img src={images[1] }  className='absolute bottom-vh5 left-vw2 w-p40 h-p50 object-cover object-center rounded-2xl'  alt='img_1'  /> */}
+                                <img src={images[0]} className='w-full h-full object-cover object-center rounded-lg'  />
 
                             </section>
                         </div>
                          </section>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className='h-p100'>
+                <SwiperSlide className='h-p100 '>
+                    <div className='w-full h-full  relative flex items-center'>
+                        <section className='w-full  h-p80  bg-primary rounded-2xl '>
+                            
+                             
+                             </section>
+                        <section className='w-full h-full absolute top-0 right-0   flex items-center px-vw2 '> 
+                        
+                        <div className='flex  h-full w-full   items-center '>
+                            <section className='flex items-start text-white '>
+                                {/* <span className='text-9xl'>"</span> */}
+                                <div className='flex flex-col gap-vh4 '>
+                                    <p className='text-xl'>Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.</p>
+                                    <h1 className='text-4xl font-bold'>Randi & Ishan</h1>
+                                </div>
+                            </section>
+                            <section className='  w-full h-full  relative border'>
+                                {/* <img src={images[0]} className='absolute top-0 right-0 h-full w-p65 object-cover rounded-3xl object-center' alt='img_1'  />
+                                <img src={images[1] }  className='absolute bottom-vh5 left-vw2 w-p40 h-p50 object-cover object-center rounded-2xl'  alt='img_1'  /> */}
+                                <img src={images[0]} className='w-full h-full object-cover object-center rounded-lg'  />
+
+                            </section>
+                        </div>
+                         </section>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-p100 '>
+                    <div className='w-full h-full  relative flex items-center'>
+                        <section className='w-full  h-p80  bg-primary rounded-2xl '>
+                            
+                             
+                             </section>
+                        <section className='w-full h-full absolute top-0 right-0   flex items-center px-vw2 '> 
+                        
+                        <div className='flex  h-full w-full   items-center '>
+                            <section className='flex items-start text-white '>
+                                {/* <span className='text-9xl'>"</span> */}
+                                <div className='flex flex-col gap-vh4 '>
+                                    <p className='text-xl'>Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.</p>
+                                    <h1 className='text-4xl font-bold'>Randi & Ishan</h1>
+                                </div>
+                            </section>
+                            <section className='  w-full h-full  relative border'>
+                                {/* <img src={images[0]} className='absolute top-0 right-0 h-full w-p65 object-cover rounded-3xl object-center' alt='img_1'  />
+                                <img src={images[1] }  className='absolute bottom-vh5 left-vw2 w-p40 h-p50 object-cover object-center rounded-2xl'  alt='img_1'  /> */}
+                                <img src={images[0]} className='w-full h-full object-cover object-center rounded-lg'  />
+
+                            </section>
+                        </div>
+                         </section>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-p100 '>
+                    <div className='w-full h-full  relative flex items-center'>
+                        <section className='w-full  h-p80  bg-primary rounded-2xl '>
+                            
+                             
+                             </section>
+                        <section className='w-full h-full absolute top-0 right-0   flex items-center px-vw2 '> 
+                        
+                        <div className='flex  h-full w-full   items-center '>
+                            <section className='flex items-start text-white '>
+                                {/* <span className='text-9xl'>"</span> */}
+                                <div className='flex flex-col gap-vh4 '>
+                                    <p className='text-xl'>Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.</p>
+                                    <h1 className='text-4xl font-bold'>Randi & Ishan</h1>
+                                </div>
+                            </section>
+                            <section className='  w-full h-full  relative border'>
+                                {/* <img src={images[0]} className='absolute top-0 right-0 h-full w-p65 object-cover rounded-3xl object-center' alt='img_1'  />
+                                <img src={images[1] }  className='absolute bottom-vh5 left-vw2 w-p40 h-p50 object-cover object-center rounded-2xl'  alt='img_1'  /> */}
+                                <img src={images[0]} className='w-full h-full object-cover object-center rounded-lg'  />
+
+                            </section>
+                        </div>
+                         </section>
+                    </div>
+                </SwiperSlide>
+                {/* <SwiperSlide className='h-p100'> */}
                     <div className='w-full h-full  relative flex items-center'>
                         <section className='w-full h-p80  bg-primary rounded-2xl '>
                             
@@ -74,8 +153,8 @@ const HomeReviewDesktop = (props: Props) => {
                         </div>
                          </section>
                     </div>
-                </SwiperSlide>
-                <SwiperSlide className='h-p100'>
+                {/* </SwiperSlide> */}
+                {/* <SwiperSlide className='h-p100'> */}
                     <div className='w-full h-full  relative flex items-center'>
                         <section className='w-full h-p80  bg-primary rounded-2xl '>
                             
@@ -99,7 +178,7 @@ const HomeReviewDesktop = (props: Props) => {
                         </div>
                          </section>
                     </div>
-                </SwiperSlide>
+                {/* </SwiperSlide> */}
                 
                
             </Swiper>
--- src/components/portfolio/CategoryCard.tsx
+++ src/components/portfolio/CategoryCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const CategoryCard = (props: Props) => {
 
     return (
-        <div className={`w-p32 py-vh2 flex flex-col justify-between items-center gap-vh1  bg-primary_light1 rounded-xl hover:bg-primary ${props.selectedLogic && ('bg-primary')}    hover:text-white transition-all duration-500`} onClick={() => props.onClickAction(props.title)}>
+        <div className={`w-p32 py-vh2 flex flex-col justify-between items-center gap-vh1  bg-primary_light1 rounded-xl hover:bg-primary cursor-pointer  ${props.selectedLogic && ('bg-primary')}    hover:text-white transition-all duration-500`} onClick={() => props.onClickAction(props.title)}>
             <span className='bg-service_svg_bg p-[8px] rounded-2xl flex items-center justify-center text-white'><GiBigDiamondRing /></span>
             <section>
                 <h3 className='font-semibold w-full text-center'>{props.title}</h3>
